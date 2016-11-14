99e3eb0e8d0d63df960a8afaeab4cdccad84492c
Amir <temirsultanovas@gmail.com>
2016-11-14 11:39:50 +0300

    third.one commit

--- Main.java
+++ Main.java
@@ -18,8 +18,16 @@ public class Main {
         new Main().incrementId();
 
         Main.globalid = 8;//отностится к классу а не к объекту
-    }
 
+        System.out.println(Main.mymethod3());//extract variable ->>
+
+        String var1 = Main.mymethod3();
+        System.out.println(var1);
+    }
+    private static String mymethod3() {
+        String id2 = "lhflkjhfd;l";
+        return id2;
+    }
     private static void mymethod() {
         System.out.println("lhflkjhfd;l");
     }
