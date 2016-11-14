b2075199448a01c72e58013042ae398ffee85da1
Amir <temirsultanovas@gmail.com>
2016-11-14 10:17:41 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        mymethod();
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+    }
+
+    private static void mymethod() {
+        System.out.println("lhflkjhfd;l");
+    }
+}
